9efb38c81564d411ba83230a8d068c0c0df2c2b1
Florian Bougel <florianbougel@gmail.com>
2018-10-06 19:21:13 +0200

    Updates

---
+++ precache-manifest.66fc429286c63bc95ddd1883b52a3513.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d51f121f5454047ceea",
+    "url": "/price-chart/static/js/runtime~main.6d51f121.js"
+  },
+  {
+    "revision": "5ab35f1b9ff04c1dae10",
+    "url": "/price-chart/static/js/main.5ab35f1b.chunk.js"
+  },
+  {
+    "revision": "573d5d004e57417f49cb",
+    "url": "/price-chart/static/js/1.573d5d00.chunk.js"
+  },
+  {
+    "revision": "9d1115d26545dc0819d5072f42051583",
+    "url": "/price-chart/index.html"
+  }
+];
